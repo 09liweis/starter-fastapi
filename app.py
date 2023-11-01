f68b124293076284ab5230b66f070b26681d2824
Replit user <>
2023-11-01 15:33:19 +0000

    integrate with mongodb, get todos data

--- app.py
+++ app.py
@@ -3,33 +3,50 @@ from fastapi.responses import FileResponse
 
 from pydantic import BaseModel
 
+from pymongo import MongoClient
+
+import os
+
+mongodb_url = os.environ['MONGODB_URL']
+
+client = MongoClient(mongodb_url)
+todos_collection = client.heroku_6njptcbp.todos
+
 app = FastAPI()
 
 
 class Item(BaseModel):
-    item_id: int
+  item_id: int
 
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+  count = todos_collection.count_documents({})
+  pending_count = todos_collection.count_documents({"status": "pending"})
+  done_count = todos_collection.count_documents({"status": "done"})
+  return {
+      "name": "todo",
+      "total": count,
+      "pending": pending_count,
+      "done": done_count
+  }
 
 
 @app.get('/favicon.ico', include_in_schema=False)
 async def favicon():
-    return FileResponse('favicon.ico')
+  return FileResponse('favicon.ico')
 
 
 @app.get("/item/{item_id}")
 async def read_item(item_id: int):
-    return {"item_id": item_id}
+  return {"item_id": item_id}
 
 
 @app.get("/items/")
 async def list_items():
-    return [{"item_id": 1, "name": "Foo"}, {"item_id": 2, "name": "Bar"}]
+  return [{"item_id": 1, "name": "Foo"}, {"item_id": 2, "name": "Bar"}]
 
 
 @app.post("/items/")
 async def create_item(item: Item):
-    return item
+  return item
