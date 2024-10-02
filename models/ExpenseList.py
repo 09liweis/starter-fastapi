aad4ab14680eb8106db5736ad68d5e864a34b8c2
Sam Li <weisen.li@hotmail.com>
2024-10-02 13:47:35 +0000

    add comment

--- models/ExpenseList.py
+++ models/ExpenseList.py
@@ -10,21 +10,25 @@ class ExpenseList:
       date = expense["date"]
       price = expense["price"]
 
+      # year expense
       if (yearDate := date[:4]) not in result_details:
         result_details[yearDate] = {"total": price}
       else:
         result_details[yearDate]["total"] += price
 
+      #year category expense
       if (category := expense["category"]) in result_details[yearDate]:
         result_details[yearDate][category] += price
       else:
         result_details[yearDate][category] = price
 
+      #month expense
       if (yearMonthDate := date[:7]) not in result_details:
         result_details[yearMonthDate] = {"total": price}
       else:
         result_details[yearMonthDate]["total"] += price
 
+      #month category expense
       if category in result_details[yearMonthDate]:
         result_details[yearMonthDate][category] += price
       else:
