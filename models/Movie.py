511bdf14c0e5c199d53d629ed4451466c88226bf
Sam Li <weisen.li@hotmail.com>
2024-11-07 19:36:49 +0000

    initial movie class

---
+++ models/Movie.py
@@ -0,0 +1,12 @@
+class Movie:
+  def __init__(self, movie):
+    self.douban_id = movie['douban_id']
+    self.title = movie['title']
+    self.year = movie['year']
+    self.current_episode = movie['current_episode']
+    self.episodes = movie['episodes']
+    self.type = movie['visual_type']
+    self.imdb_id = movie['imdb_id']
+    self.genres = movie['genres']
+    self.countries = movie['countries']
+    self.languages = movie['languages']
