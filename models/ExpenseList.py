45045b16f7ff9e9c1b1d36e06594049db317ad95
Sam Li <weisen.li@hotmail.com>
2025-01-21 18:33:07 +0000

    refactor expenselist

--- models/ExpenseList.py
+++ models/ExpenseList.py
@@ -7,38 +7,23 @@ class ExpenseList:
     self.expenses = expenses
 
   def getExpenseCounts(self):
-    expenses = self.expenses
     result_details = {}
-    for expense in expenses:
+    for expense in self.expenses:
       ex = Expense(expense)
       yearDate = ex.getYear()
       yearMonthDate = ex.getMonth()
-      
       price = ex.getPrice()
-
       category = ex.getCategory()
-
-      # year expense
-      if yearDate not in result_details:
-        result_details[yearDate] = {"total": price}
-      else:
-        result_details[yearDate]["total"] += price
-
-      #year category expense
-      if category in result_details[yearDate]:
-        result_details[yearDate][category] += price
-      else:
-        result_details[yearDate][category] = price
-
-      #month expense
-      if yearMonthDate not in result_details:
-        result_details[yearMonthDate] = {"total": price}
-      else:
-        result_details[yearMonthDate]["total"] += price
-
-      #month category expense
-      if category in result_details[yearMonthDate]:
-        result_details[yearMonthDate][category] += price
-      else:
-        result_details[yearMonthDate][category] = price
+      self._add_expense_to_details(result_details, yearDate, price, category)
+      self._add_expense_to_details(result_details, yearMonthDate, price, category)
     return {"name": "expense", "details": result_details}
+
+  def _add_expense_to_details(self, result_details, date, price, category):
+      if date not in result_details:
+        result_details[date] = {"total": price}
+      else:
+        result_details[date]["total"] += price
+      if category in result_details[date]:
+        result_details[date][category] += price
+      else:
+        result_details[date][category] = price
